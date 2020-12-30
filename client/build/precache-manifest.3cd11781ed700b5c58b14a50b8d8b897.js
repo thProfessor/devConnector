c37c73c9925a3aae42ed2b93ad651a0f03c73d92
Utkarsh Shukla <55681231+thProfessor@users.noreply.github.com>
2020-12-30 13:03:21 +0530

    Add files via upload

---
+++ client/build/precache-manifest.3cd11781ed700b5c58b14a50b8d8b897.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7733f5aea1a5b9031fa2fa21857bc196",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33c0905bc0b05fd939cc",
+    "url": "/static/css/main.48389a3a.chunk.css"
+  },
+  {
+    "revision": "7a47cefa0038018ef82b",
+    "url": "/static/js/2.c16a21dd.chunk.js"
+  },
+  {
+    "revision": "005c97f82cee60e20eac659314ea29ec",
+    "url": "/static/js/2.c16a21dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33c0905bc0b05fd939cc",
+    "url": "/static/js/main.b0c2c4f0.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
